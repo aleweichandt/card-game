3e259bba17da3bd2348e4941e6319c6011022c2e
Alejandro Weichandt <alejandro.weichandt@lastminute.com>
2023-03-29 22:53:55 +0200

    ignore error

--- src/modules/core/model/init.ts
+++ src/modules/core/model/init.ts
@@ -4,6 +4,7 @@ export const init = async (
   socketUrl: string = '/api/socket',
 ): Promise<Socket> => {
   await fetch(socketUrl)
+  // @ts-ignore
   return io(process.env.BASE_URL, {
     autoConnect: false,
     path: "/api/socket",
