13e94a6a89ed56db5f5b11eb54f0eba01ef0c084
Alejandro Weichandt <alejandro.weichandt@lastminute.com>
2023-03-29 22:51:31 +0200

    test with baseurl

--- src/modules/core/model/init.ts
+++ src/modules/core/model/init.ts
@@ -4,7 +4,7 @@ export const init = async (
   socketUrl: string = '/api/socket',
 ): Promise<Socket> => {
   await fetch(socketUrl)
-  return io( {
+  return io(process.env.BASE_URL, {
     autoConnect: false,
     path: "/api/socket",
   });
